ba662f4da2438ee0c6f44c64d32c2a7bd61ab686
Abhinay1998 <abhinaypeeta@my.unt.edu>
2025-03-30 22:53:50 -0500

    Axisbottom.js

--- Axisbottom.js
+++ Axisbottom.js
@@ -1,9 +1,23 @@
-export const AxisBottom = ({ xScale, innerHeight, tickFormat }) =>
-    xScale.ticks().map(tickValue => (
-      <g className="tick" key={tickValue} transform={`translate(${xScale(tickValue)},0)`}>
-        <line y2={innerHeight} />
-        <text style={{ textAnchor: 'middle' }} dy=".71em" y={innerHeight + 3}>
-          {tickFormat(tickValue)}
-        </text>
-      </g>
-    ));
+import React from 'react';
+
+export const AxisBottom = ({
+  xScale,
+  innerHeight,
+  tickFormat,
+}) =>
+  xScale.ticks().map((tickValue) => (
+    <g
+      className="tick"
+      key={tickValue}
+      transform={`translate(${xScale(tickValue)},0)`}
+    >
+      <line y2={innerHeight} />
+      <text
+        style={{ textAnchor: 'middle' }}
+        dy=".71em"
+        y={innerHeight + 3}
+      >
+        {tickFormat(tickValue)}
+      </text>
+    </g>
+  ));
