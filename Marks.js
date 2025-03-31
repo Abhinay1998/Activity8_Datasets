34b3bb19e9e80f8bb39b5a72e042196a012e37e7
Abhinay1998 <abhinaypeeta@my.unt.edu>
2025-03-30 22:58:11 -0500

    Marks.js

--- Marks.js
+++ Marks.js
@@ -1,21 +1,23 @@
+import React from 'react';
+
 export const Marks = ({
-    data,
-    xScale,
-    yScale,
-    xValue,
-    yValue,
-    tooltipFormat
-  }) =>
-    data.map(d => (
-      <rect
-        className="mark"
-        key={yValue(d)}
-        x={0}
-        y={yScale(yValue(d))}
-        width={xScale(xValue(d))}
-        height={yScale.bandwidth()}
-      >
-        <title>{tooltipFormat(xValue(d))}</title>
-      </rect>
-    ));
+  data,
+  xScale,
+  yScale,
+  xValue,
+  yValue,
+  tooltipFormat,
+}) =>
+  data.map((d) => (
+    <rect
+      className="mark"
+      key={yValue(d)}
+      x={0}
+      y={yScale(yValue(d))}
+      width={xScale(xValue(d))}
+      height={yScale.bandwidth()}
+    >
+      <title>{tooltipFormat(xValue(d))}</title>
+    </rect>
+  ));
   
